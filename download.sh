bcc40ae301e45b2a50aa4201b2ae86f794a2b5b5
Stefan Baebler <stefan.baebler@gmail.com>
2021-08-12 08:25:34 +0200

    Generate markdown list of all rules per country

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ rm -rf "rules.bak"
 mv "rules" "rules.bak" || true
 mkdir -p "rules"
 
-cat << ENDHEADER >> rules/README.md
+cat << ENDHEADER > "rules/README.md"
 # List of rules
 
 | Country | Rule ID | File | Source | Description |
@@ -17,10 +17,22 @@ ENDHEADER
 curl -s "${BASEURL}" | jq -r '.[] | .country + " " + .identifier + " " + .hash ' \
 | while IFS=" " read -r COUNTRY ID HASH; do
     mkdir -p "rules/${COUNTRY}"
+
+    if [ ! -f "rules/${COUNTRY}/README.md" ]
+    then
+        cat << ENDCOUTRYHEADER > "rules/${COUNTRY}/README.md"
+# List of rules for country ${COUNTRY}
+
+| Rule ID | File | Source | Description |
+| ------- | ---- | ------ | ----------- |
+ENDCOUTRYHEADER
+    fi
+
     echo -n "Downloading ${COUNTRY}: ${ID} > "
     curl -s "${BASEURL}${COUNTRY}/${HASH}" | jq --sort-keys > "rules/${COUNTRY}/${ID}.json"
     DESC=$(jq -r 'select(.Description != null) | .Description[]|select(.lang == "en").desc' "rules/${COUNTRY}/${ID}.json")
     echo "${DESC}"
-    echo "| ${COUNTRY} |${ID} | [JSON](${COUNTRY}/${ID}.json) | [API](${BASEURL}${COUNTRY}/${HASH}) | ${DESC} |" >> rules/README.md
+    echo "| ${COUNTRY} |${ID} | [JSON](${COUNTRY}/${ID}.json) | [API](${BASEURL}${COUNTRY}/${HASH}) | ${DESC} |" >> "rules/README.md"
+    echo "| ${ID} | [JSON](${COUNTRY}/${ID}.json) | [API](${BASEURL}${COUNTRY}/${HASH}) | ${DESC} |" >> "rules/${COUNTRY}/README.md"
 done
 rm -rf "rules.bak"
